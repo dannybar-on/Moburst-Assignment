79773c10914ac28fe3fa39b6ecea240ab5b94777
dannybar-on <93839751+dannybar-on@users.noreply.github.com>
2022-08-10 22:34:11 +0300

    finalize

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
